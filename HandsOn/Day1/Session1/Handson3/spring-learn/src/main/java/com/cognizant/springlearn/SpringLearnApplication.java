c3a3d502c1c000d2ae41e5c2c718d5df48064f67
jahanvi7 <47592886+jahanvi7@users.noreply.github.com>
2021-02-21 17:34:00 +0530

    Add files via upload

---
+++ HandsOn/Day1/Session1/Handson3/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -0,0 +1,40 @@
+package com.cognizant.springlearn;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import org.slf4j.Logger;
+
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnApplication.class, args);
+		SpringLearnApplication s = new SpringLearnApplication();
+		s.displayDate();
+	}
+	
+	public void displayDate() {
+		LOGGER.info("START");
+		ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+		SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+		String dateAsString = "31/12/2018";
+		try {
+			System.out.println(format.parse(dateAsString));
+			LOGGER.info(dateAsString);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		LOGGER.info("END");
+	}
+
+}
